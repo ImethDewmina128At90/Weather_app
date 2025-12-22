5b509ef19207310be59289aaa4f0b8d22c63aa9c
Imeth Dewmina <imeth.20240076@iit.ac.lk>
2025-12-22 12:22:40 +0530

    lerned about nodemon

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const {name,add} = require('./utils.js');
-const getNotes = require('./notes.js')
-const vali = require('validator')
+// const {name,add} = require('./utils.js');
+// const getNotes = require('./notes.js')
+// const vali = require('validator')
 
 
-console.log(name);
+// console.log(name);
 
+// console.log(add(5, 3))
 
-console.log(add(5, 3))
+// const msg = getNotes()
 
-const msg = getNotes()
+// console.log(msg)
 
-console.log(msg)
+// console.log("just checking email " + vali.isEmail('imeth128@gmail.com'))
 
-console.log(vali.isEmail('imeth128@gmail.com'))
+const chalk = require('chalk')  
 
+console.log(chalk.green.italic.bgCyan('Sucesss'))
 
 
 
