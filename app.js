6dc035914971bada4055ed8130f673a386b440da
Imeth Dewmina <imeth.20240076@iit.ac.lk>
2025-12-22 11:33:41 +0530

    downloaded node moudle validator

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const {name,add} = require('./utils.js');
 const getNotes = require('./notes.js')
+const vali = require('validator')
+
 
 console.log(name);
 
@@ -9,6 +11,8 @@ console.log(add(5, 3))
 const msg = getNotes()
 
 console.log(msg)
+
+console.log(vali.isEmail('imeth128@gmail.com'))
 
 
 
