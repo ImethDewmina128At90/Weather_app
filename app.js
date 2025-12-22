e2dcf7fcc98649c9b84d83e534d9ffc8be06baa8
Imeth Dewmina <imeth.20240076@iit.ac.lk>
2025-12-22 13:09:19 +0530

    addded yargs and completed a sample

--- app.js
+++ app.js
@@ -13,11 +13,13 @@
 
 // console.log("just checking email " + vali.isEmail('imeth128@gmail.com'))
 
-const chalk = require('chalk')  
+const chalk = require('chalk')
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 
 console.log(chalk.green.italic.bgCyan('Sucesss'))
 
-console.log(process.argv)
-
+console.log('Raw argv:', process.argv)
+console.log('Parsed argv:', yargs(hideBin(process.argv)).argv)
 
 
