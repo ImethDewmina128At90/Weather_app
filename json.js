dad09f3fed5c2a53fb66d8d53244585df9c003f5
Imeth Dewmina <imeth.20240076@iit.ac.lk>
2025-12-23 18:31:54 +0530

    parse

--- json.js
+++ json.js
@@ -1,9 +1,21 @@
 const fs = require('fs')
 
+// const dataBufferr = fs.readFileSync('json.json')
+
+// const dataJSON = dataBufferr.toString()
+
+// const data =JSON.parse(dataJSON)
+
+// console.log(data.author)
+
+
 const dataBufferr = fs.readFileSync('json.json')
 
-const dataJSON = dataBufferr.toString()
+const dataJSON = dataBufferr.toString() 
 
-const data =JSON.parse(dataJSON)
+const data_m = JSON.parse(dataJSON)
+data_m.age = 23
 
-console.log(data.author)
+const userJSON = JSON.stringify(data_m)
+
+fs.writeFileSync('json.json', userJSON)
