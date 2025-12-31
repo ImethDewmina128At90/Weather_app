e119f379f33c7c11eb71455c37e70491995575e7
Imeth Dewmina <imeth.20240076@iit.ac.lk>
2025-12-31 06:18:29 +0530

    update

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,6 +1,6 @@
 const request = require('request')
 
-const URL = 'https://api.weatherstack.com/current?access_key=c1bf736e89dec5bbd419016db06f3479&query=7.0933385,2079.993689&units=m'
+const URL = 'https://api.weatherstack.com/current?access_key=c1bf736e89dec5bbd419016db06f3479&query=7.0933385,%2079.993689&units=m'
 
 
 request({ url: URL,json:true }, (error,response) => {
